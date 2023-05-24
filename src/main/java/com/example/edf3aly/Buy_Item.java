58ba18804ea2a1b322edf089f1350065475d4cbf
Tamer Ihab <tamerihab02@gmail.com>
2023-05-24 23:18:48 +0200

    updated

--- src/main/java/com/example/edf3aly/Buy_Item.java
+++ src/main/java/com/example/edf3aly/Buy_Item.java
@@ -1,10 +1,12 @@
 package com.example.edf3aly;
 
+import java.util.Date;
+
 public class Buy_Item extends Transactions {
     private String store;
     public Buy_Item(int transactionID, double amount, String store)
     {
-        super(transactionID, "Buy", amount);
+        super(transactionID, "Buy", amount,new Date());
         this.store = store;
     }
     public String getStore()
